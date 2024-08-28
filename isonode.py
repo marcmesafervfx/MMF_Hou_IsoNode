6fa33ccd7e31a636f0c595f9006771f1d22e5e57
Marc Mesa Fernandez <134970351+marcmesafervfx@users.noreply.github.com>
2024-08-28 08:25:09 +0200

    Update isonode
    Just a quick check. No visible updates.

--- isonode.py
+++ isonode.py
@@ -22,29 +22,49 @@ def isoGeo():
     # Initialize the Root object node and its children    
     OBJ_ROOT = hou.node("/obj/")
     children = OBJ_ROOT.children()
-    children = [ x for x in children if x.type().name()=="geo"]    
+    
+    child_nodes = []
+    
+    for child in children:
+        try:
+            if not "light" in child.type().name(): 
+                child.isDisplayFlagSet()
+                child_nodes.append(child)
+        except:
+            pass
+      
     
     # Initialize current visible nodes.
     visible = []
     
     # Check which nodes are visible and append them to the list.
-    for node in children:
+    for node in child_nodes:
         if node.isDisplayFlagSet():
             visible.append(node)
     
     # Get the selected nodes.        
     selected = hou.selectedNodes()
+
+    sel_nodes = []
+
+    for sel in selected:
+        try:
+            if not "light" in child.type().name(): 
+                sel.isDisplayFlagSet()
+                sel_nodes.append(sel)
+        except:
+            pass
     
     # Check if the isolation mode and set flags based on response.
     if hou.session.ISO_MODE == "OFF":
         hou.session.PREV_ISO = visible
         hou.session.ISO_MODE = "ON"
-        for node in children:
+        for node in child_nodes:
             node.setDisplayFlag(0)
         for select in selected:
             select.setDisplayFlag(1)
     else:
-        for node in children:
+        for node in child_nodes:
             node.setDisplayFlag(0)
         for node in hou.session.PREV_ISO:
             node.setDisplayFlag(1)
